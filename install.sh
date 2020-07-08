d31ac6ce8d0f093dcb04df219f8b23486f55bf10
Mihael Savin <jtprogru@gmail.com>
2020-07-08 23:55:20 +0300

    Some magic

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-sed -i "s~dbms_template_path~$project_path~g" nginx/site.conf systemd/gunicorn.service
-sed -i "s~dbms_template_domain~$project_domain~g" nginx/site.conf src/config/settings.py
+sed -i "s~TEMPLATE_PATH~$project_path~g" nginx/site.conf systemd/gunicorn.service
+sed -i "s~TEMPLATE_DOMAIN~$project_domain~g" nginx/site.conf src/config/settings.py
 
 sudo ln -s $project_path/nginx/site.conf /etc/nginx/sites-enabled/
 sudo ln -s $project_path/systemd/gunicorn.service /etc/systemd/system/
