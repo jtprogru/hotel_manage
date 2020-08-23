1bc428035fd82a397dd8a3647c0d6f390d2e2eca
JTProg <jtprogru@gmail.com>
2020-08-23 22:03:07 +0300

    Rewrite client models - remove Passport model;

--- src/clients/admin.py
+++ src/clients/admin.py
@@ -1,54 +1,27 @@
 from django.contrib import admin
-from .models import Client, Passport
-
-
-class ClientInline(admin.StackedInline):
-    model = Client
-    extra = 1
-
-
-class PassportInline(admin.StackedInline):
-    model = Passport
-    extra = 1
-
-
-class ClientAdmin(ClientInline):
-    """Клиенты"""
-    list_display = ("second_name", "first_name", "middle_name", "status", "is_vip")
-    list_filter = ("status", "is_vip")
-    inlines = ['PassportInline']
-    fieldsets = (
-        (None, {
-            "fields": (("second_name", "first_name", "middle_name"),)
-        }),
-        (None, {
-            "fields": (("status", "is_vip"),)
-        }),
-    )
-
-
-class PassportAdmin(PassportInline):
-    """Паспорта"""
-    list_display = ("serial", "number", "police_department", "police_department_id",
-                    "date_issue", "sex", "birth_date", "birth_place", "registration")
-    fieldsets = (
-        (None, {
-            "fields": (("serial", "number"),)
-        }),
-        (None, {
-            "fields": (("police_department",),)
-        }),
-        (None, {
-            "fields": (("date_issue", "police_department_id"),)
-        }),
-        (None, {
-            "fields": (("sex", "birth_date"), "birth_place")
-        }),
-        (None, {
-            "fields": (("registration",),)
-        }),
-    )
-
+from .models import Client
 
 admin.site.register(Client)
-admin.site.register(Passport)
+
+# class ClientInline(admin.StackedInline):
+#     model = Client
+#     extra = 1
+
+
+# class PassportInline(admin.StackedInline):
+#     model = Passport
+#     extra = 1
+
+
+# class ClientAdmin(ClientInline):
+#     """Клиенты"""
+#     list_display = ("second_name", "first_name", "middle_name", "status", "is_vip")
+#     list_filter = ("status", "is_vip")
+#     fieldsets = (
+#         (None, {
+#             "fields": (("second_name", "first_name", "middle_name"),)
+#         }),
+#         (None, {
+#             "fields": (("status", "is_vip"),)
+#         }),
+#     )
---
+++ src/clients/form.py
@@ -0,0 +1,14 @@
+from django.db.models import Q
+from django import forms
+
+from .models import Passport, Client
+
+
+class PassportCreationForm(forms.ModelForm):
+    class Meta:
+        model = Passport
+        fields = ["serial", "number", "police_department", "police_department_id",
+                  "date_issue", "sex", "birth_date", "birth_place", "registration"]
+
+    # def date_of_birth(self):
+    #     return self.birth_date
