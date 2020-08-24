46eef9da719b64b44efcd6de77c188913c3f3e04
JTProg <jtprogru@gmail.com>
2020-08-24 13:01:44 +0300

    Clear before start working with DRF;

--- src/apartments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.ApartmentView.as_view()),
-    path("<int:pk>/", views.AddApartment.as_view(), name="add_apartment"),
-]
--- src/clients/models.py
+++ src/clients/models.py
@@ -31,7 +31,6 @@ class Client(models.Model):
     first_name = models.CharField(verbose_name="Имя", max_length=64)
     middle_name = models.CharField(verbose_name="Отчество", max_length=64)
     second_name = models.CharField(verbose_name="Фамилия", max_length=64)
-    status = models.CharField(verbose_name="Статус", max_length=64)
     is_vip = models.BooleanField(verbose_name="VIP-статус", default=False)
     serial = models.PositiveIntegerField(verbose_name='Серия паспорта')
     number = models.PositiveIntegerField(verbose_name='Номер паспорта')
@@ -56,51 +55,3 @@ class Client(models.Model):
         verbose_name = "Клиент"
         verbose_name_plural = "Клиенты"
         db_table = "clients"
-
-
-# class Passport(models.Model):
-#     """
-#     Модель паспорта клиента
-#
-#     :param int id: - Уникальный ID в рамках таблицы
-#     :param int serial: - Серия паспорта
-#     :param int number: - Номер паспорта
-#     :param str police_department: - Кем выдано
-#     :param int police_department_id: - Код подразделения
-#     :param date date_issue: - Дата выдачи
-#     :param str sex: - Пол
-#     :param date birth_date: - Дата рождения
-#     :param date birth_place: - Место рождения
-#     :param str registration: - Адрес регистрации
-#     :param byte image: - Скан паспорта TODO: Доделать возможность прикреплять скан парспорта
-#     """
-#     MALE = 'male'
-#     FEMAIL = 'femail'
-#     SEX_CHOICES = [
-#         (MALE, 'муж'),
-#         (FEMAIL, 'жен'),
-#     ]
-#     # passport_id = models.IntegerField(verbose_name="ID пасспорта", primary_key=True, unique=True)
-#     serial = models.PositiveIntegerField(verbose_name='Серия паспорта')
-#     number = models.PositiveIntegerField(verbose_name='Номер паспорта')
-#     police_department = models.CharField(verbose_name='Кем выдан', max_length=250)
-#     police_department_id = models.PositiveIntegerField(verbose_name='Код подразделения')
-#     date_issue = models.DateField(verbose_name='Дата выдачи')
-#     sex = models.CharField(verbose_name="Пол",
-#                            max_length=10,
-#                            choices=SEX_CHOICES,
-#                            default=MALE)
-#     birth_date = models.DateField(verbose_name='Дата рождения')
-#     birth_place = models.CharField(verbose_name='Место рождения', max_length=300)
-#     registration = models.CharField(verbose_name='Адрес регистрации', max_length=450)
-#
-#     def __str__(self):
-#         return f'Серия: {self.serial} Номер: {self.number}\nПол: {self.get_sex()}'
-#
-#     def get_sex(self):
-#         return 'муж' if self.sex == 'male' else 'жен'
-#
-#     class Meta:
-#         verbose_name = "Паспорт"
-#         verbose_name_plural = "Паспорта"
-#         db_table = "passports"
